06f98a2679d0d0fdd756cedabb66a7fa16bd9294
ChoucheneHayfa <hayfa.chouchene1@mail.com>
2022-03-12 02:18:02 +0100

    My first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Quote from './components/Quote';
+
+function App() {
+  const [quote, setQuote] = useState({
+    anime: null,
+    character: null,
+    quote: null
+  })
+
+const generate=async()=>{
+  setQuote(await fetchQuote());
+}
+
+  const fetchQuote = async () => {
+    return await fetch('https://animechan.vercel.app/api/random')
+      .then(response => response.json());
+  }
+useEffect(async () =>{
+  setQuote(await fetchQuote());
+
+},[])
+
+  return (
+    <div className="App">
+      <Quote quote={quote}/>
+      <button  onClick={generate}>Generate a new quote</button>
+    </div>
+  );
+}
+
+export default App;
